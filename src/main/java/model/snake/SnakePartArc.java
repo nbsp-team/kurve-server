5bfae1680bf7ea5cbbdbb32ec4822702c42f308e
Nickolay Chameyev <lukaville@gmail.com>
2015-02-22 15:10:09 +0300

    SnakePartArc angle field

--- src/main/java/model/snake/SnakePartArc.java
+++ src/main/java/model/snake/SnakePartArc.java
@@ -14,12 +14,50 @@ public class SnakePartArc extends SnakePart {
     private Point pointFrom;
     private float radius;
     private Direction direction;
-    private float length;
+    private float angle;
 
-    public SnakePartArc(Point pointFrom, float radius, Direction direction, float length) {
+    public SnakePartArc(Point pointFrom, float radius, Direction direction, float angle) {
         this.pointFrom = pointFrom;
         this.radius = radius;
         this.direction = direction;
-        this.length = length;
+        this.angle = angle;
+    }
+
+    public Point getPointFrom() {
+        return pointFrom;
+    }
+
+    public void setPointFrom(Point pointFrom) {
+        this.pointFrom = pointFrom;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        if (angle >= 0 && angle <= 2 * Math.PI) {
+            this.angle = angle;
+        } else {
+            throw new IllegalArgumentException(
+                    "Angle must be larger than 0 and smaller than 2*pi"
+            );
+        }
     }
 }
